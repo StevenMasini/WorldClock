6bb9813639e6b9a8e6a01ba8f767378d02953f5a
Steven Masini <steven.masini@gmail.com>
2015-08-03 21:05:15 +0800

    First commit

---
+++ JediClock/JediClock/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JediClock
+//
+//  Created by Steven Masini on 8/3/15.
+//  Copyright (c) 2015 Steven Masini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JediClock/JediClock/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  JediClock
+//
+//  Created by Steven Masini on 8/3/15.
+//  Copyright (c) 2015 Steven Masini. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
